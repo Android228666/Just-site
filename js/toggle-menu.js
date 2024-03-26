e4e7cab846380e764b4f3bf24bedae187d707862
Andrii Khomenko <asus261235@gmail.com>
2024-03-26 12:56:45 +0100

    Toggle, and hero - done!

---
+++ js/toggle-menu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("header-nav");
+    menu.classList.toggle("active");
+   /* if(menu.style.maxHeight){
+        menu.style.maxHeight = null;
+    }else{
+        menu.style.maxHeight = menu.scrollHeight + "px";
+    }*/
+}
